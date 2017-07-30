51f9e520050d2fbf741ad405fd4b05b444e986df
glenn <glennrcontreras@gmail.com>
2017-07-30 10:36:43 -0700

    added levels.json and made some changes to parser

--- parser.py
+++ parser.py
@@ -1,34 +1,52 @@
 import pandas as pd
 
+# the classes should match the folder names that contain the training sets
+def construct_csv_paths(parent, classes):
 
-def append_data_arff(data, category, path):
-    with open(path, mode='a') as f:
-        f.write("@DATA\n")
-        for index, row in data.iterrows():
-            f.write("{}, {}\n".format(",".join(map(str, row)), category))
 
 
 def main():
-    swing_csv_path = "swing2pos.csv"
-    swing_arff_path = "swing2.arff"
+    import json
+    config_path = "config.json"
 
-    # TODO: Write a function that reads in column info from json
-    column_names = ['frame', 'sacrum_x', 'sacrum_y']
-    data = pd.read_csv(swing_csv_path, skipinitialspace=True, usecols=column_names)
+    with open(config_path) as f:
+        config_info = json.load(f)
 
-    relation = "swing2_labeled"
-    classes = ["running", "walking", "jumping", "varied"]
+    relation = config_info["relation"]
+    classes = config_info["classes"]
+    levels_path = config_info["levels"]["path"]
 
-    with open(swing_arff_path, mode='w') as f:
-        f.write("@RELATION {}\n".format(relation))
+    with open(levels_path) as f:
+        levels_data = json.load(f)
 
-        for c in data.columns:
-            if c == "frame":
-                f.write("@ATTRIBUTE UNIQUE_ID REAL\n")
-            else:
-                f.write("@ATTRIBUTE {} REAL\n".format(c))
+    for tier, desc in levels_data:
+        output = "{}_{}.arff".format(relation, tier)
 
-        f.write("@ATTRIBUTE class {}{}{}\n".format("{", ",".join(classes), "}"))
+        columns = []
+        for j in desc["joint"]:
+            for a in desc["attr"]:
+                columns.append("{}_{}".format(j, a))
+
+        with open(output, mode='w') as f:
+            f.write("@RELATION {}\n".format(relation))
+
+            for c in columns:
+                if c == "frame":
+                    f.write("@ATTRIBUTE UNIQUE_ID REAL\n")
+                else:
+                    f.write("@ATTRIBUTE {} REAL\n".format(c))
+
+            f.write("@ATTRIBUTE class {}{}{}\n".format("{", ",".join(classes), "}"))
+
+
+        with open(path, mode='a') as f:
+            for p, cl in input_info:
+                m_data = pd.read_csv(p, skipinitialspace=True, usecols=columns)
+                f.write("@DATA\n")
+                for index, row in data.iterrows():
+                    f.write("{},{}\n".format(",".join(map(str, row)), category))
+
+
 
 
 if __name__ == "__main__":

b1a6521e653334476a31a17119d96a71775c6731
GlennRC <glennrcontreras@gmail.com>
2017-08-01 22:13:54 -0700

    Adding Data

--- parser.py
+++ parser.py
@@ -1,31 +1,32 @@
 import pandas as pd
+import json
+import glob
+import argparse as ap
 
 
 def main():
-    import json
-    import glob
-    config_path = "config.json"
+    parser = ap.ArgumentParser()
+    parser.add_argument("config_path")
+    args = parser.parse_args()
 
     # read in the config file
-    with open(config_path) as f:
-        config_info = json.load(f)
-        relation = config_info["relation"]
-        classes = config_info["classes"]
-        levels_path = config_info["levels"]["path"]
-        parent = config_info["parent"]
+    with open(args.config_path) as f:
+        info = json.load(f)
+        config = info["config"]
+        tiers = info["tiers"]
+        relation = config["relation"]
+        classes = config["classes"]
+        parent_in = config["input"]
+        parent_out = config["output"]
 
     # Get all csv file paths
     input_files = {}
     for c in classes:
-        input_files.update({c: glob.glob("./{}/{}/*.csv".format(parent, c))})
-
-    # Load in the tier data to generate columns
-    with open(levels_path) as f:
-        levels_data = json.load(f)
+        input_files.update({c: glob.glob("./{}/{}/*.csv".format(parent_in, c))})
 
     # Write an arff file for each tier
-    for tier, desc in levels_data.iteritems():
-        output = "{}_{}.arff".format(relation, tier)
+    for tier, desc in tiers.iteritems():
+        output = "{}/{}_{}.arff".format(parent_out, relation, tier)
 
         # Generate the columns which depends on the current tier
         columns = []
@@ -33,20 +34,23 @@ def main():
             for a in desc["attr"]:
                 columns.append("{}_{}".format(j, a))
 
+        # begin writing the arff file
         with open(output, mode='w') as f:
             f.write("@RELATION {}_{}\n".format(relation,tier))
 
             f.write("@ATTRIBUTE UNIQUE_ID NUMERIC\n")
             for c in columns:
-                f.write("@ATTRIBUTE {} NUMERIC\n".format(c))
+                f.write("@ATTRIBUTE {} REAL\n".format(c))
 
             f.write("@ATTRIBUTE class {}{}{}\n".format("{", ",".join(classes), "}"))
 
+            # write the data for each file
+            # count represents the unique_id
+            f.write("@DATA\n")
             count = 0
             for cl, files in input_files.iteritems():
                 for i in files:
                     m_data = pd.read_csv(i, skipinitialspace=True, usecols=columns)
-                    f.write("@DATA\n")
                     for index, row in m_data.iterrows():
                         f.write("{},{},{}\n".format(count, ",".join(map(str, row)), cl))
                         count += 1
